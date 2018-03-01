c4cd1b28317df78cb4dcd978eec086301778d516
kpadillar <m_on_ka@hotmail.com>
2018-03-01 08:29:56 -0600

    Add: Registro basico

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html5>
+<html>
+<head>
+<title>Bienvenida</title>
+<link href="css/registro.css"
+      rel="stylesheet" />
+</head>
+
+<body>
+<h2>Bienvenida</h2>
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Cras cursus, ex quis interdum suscipit, lectus elit tempus mi, 
+ultricies porta odio velit eu ligula. Aliquam non hendrerit odio, 
+vel venenatis ex. Nunc nec orci sed nunc ultricies efficitur.
+Nulla dui augue, pellentesque quis mauris vel, efficitur convallis felis. 
+Proin ac est nec massa pharetra tempus sed at nunc. Nam mattis interdum ligula
+eget tristique est interdum id. Integer mattis, turpis vel volutpat 
+sollicitudin, nibh est pharetra urna, ac mollis enim augue tincidunt turpis. 
+Aenean volutpat magna at ex accumsan, quis ultrices magna aliquet. 
+Nam dui orci, rhoncus at enim sed, maximus imperdiet velit. Nullam maximus
+lectus eget fermentum tincidunt.  
+</p>
+
+<h2>Mision</h2>
+<p>
+Sed commodo augue justo, sed imperdiet orci egestas vitae. Nullam ultrices tellus est, 
+at ultrices odio vestibulum non. Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+Sed pulvinar dolor egestas blandit tempor. Aenean eros nisi, mollis at efficitur ut, hendrerit 
+vitae nisl. Nunc non nunc eu nisl bibendum faucibus. Sed tellus erat, sodales quis interdum quis, 
+interdum ut turpis. 
+</p>
+
+<h2>Vision</h2>
+<p>
+Pellentesque mattis ac neque id iaculis. In hac habitasse platea dictumst. Vestibulum ante ipsum 
+primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis bibendum, lectus ut elementum 
+suscipit, purus diam suscipit odio, quis viverra tortor libero in massa. Aliquam erat volutpat. 
+</p>
+
+<a href = "/registro.php">Accede al area de registro</a>
+</body>
+</html>
---
+++ registro.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html5>
+<html>
+<head>
+	<title>"Registro de Usuarios"</title>
+
+<link href="css/registro.css"
+      rel="stylesheet" />
+
+</head>
+
+<body>
+<h2>"Registro de Usuarios"</h2>
+<form>
+Nombre:<input type="text" nombre="nombre"> <br>
+Apellido Paterno:<input type="text" apPaterno="Apellido"> <br>
+Apellido Materno:<input type="text" apMaterno="Apellido"> <br>
+Tel&eacute;fono:<input type="text" telefono="telefono"> <br>
+Correo: <input type="text" correo="correo"> <br>
+<input type="submit">
+</form>
+</body>
+</html>
+
+
